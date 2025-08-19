3948803ce1d76462e3253675f4cb121b3be24a82
Daniele Prada <danieleprada@hotmail.com>
2025-08-19 04:16:23 -0300

    Adiciona nova pasta com .gitkeep

---
+++ themes/globsex/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'Globsex';
+
+$themeFolder = 'globsex';
+
+$themeAuthor = 'Kulvir Singh';
+
+$themeAuthorUrl = '';
+
+$themeVirsion = '2.9';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
