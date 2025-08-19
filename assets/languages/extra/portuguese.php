a9e6d178e48819fb769526e642acac9eb0eda113
Daniele Prada <danieleprada@hotmail.com>
2025-08-19 02:48:10 -0300

    Adiciona gitignore e remove arquivos grandes

---
+++ assets/languages/extra/portuguese.php
@@ -0,0 +1,99 @@
+<?php
+$wo['countries_name'] = array(
+    '1' => 'United States',
+    '12' => 'Argentina',
+    '32' => 'Brasil',
+    '44' => 'Chile',
+    '48' => 'Colombia',
+    '93' => 'Haiti',
+    '169' => 'Paraguay',
+    '199' => 'Spain',
+    '226' => 'United Kingdom',
+    '228' => 'Uruguay',
+);
+
+
+$wo['page_categories'] = array(
+    '2' => 'Carros e Ve&iacute;culos',
+    '3' => 'Com&eacute;dia',
+    '4' => 'Com&eacute;rcio e Economia',
+    '5' => 'Educa&ccedil;&atilde;o',
+    '6' => 'Entretenimento',
+    '7' => 'Filmes e Anima&ccedil;&otilde;es',
+    '8' => 'Jogos',
+    '9' => 'Fatos e Hist&oacute;ria',
+    '10' => 'Estilo de Vida',
+    '11' => 'Natural',
+    '12' => 'Not&iacute;cias e Pol&iacute;tica',
+    '13' => 'Na&ccedil;&otilde;es e Pessoas',
+    '14' => 'Animais',
+    '15' => 'Lugares e Regi&otilde;es',
+    '16' => 'Ci&ecirc;ncia e Tecnologia',
+    '17' => 'Esporte',
+    '18' => 'Viagens e Eventos',
+    '1' => 'Outros'
+);
+
+$wo['relationship'] = array(
+    '1' => 'Solteiro',
+    '2' => 'Em um relacionamento',
+    '3' => 'Casado',
+    '4' => 'Noivo'
+);
+
+$wo['months'] = array(
+    '1' => 'Janeiro',
+    '2' => 'Fevereiro',
+    '3' => 'Mar&ccedil;o',
+    '4' => 'Abril',
+    '5' => 'Maio',
+    '6' => 'Junho',
+    '7' => 'Julho',
+    '8' => 'Agosto',
+    '9' => 'Setembro',
+    '10' => 'Outubro',
+    '11' => 'Novembro',
+    '12' => 'Dezembro'
+);
+
+$wo['call_action'] = array(
+    '1' => 'Ver mais',
+    '2' => 'Comprar agora',
+    '3' => 'Vizualizar agora',
+    '4' => 'Visitar agora',
+    '5' => 'Reservar agora',
+    '6' => 'Aprender mais',
+    '7' => 'Jogar agora',
+    '8' => 'Aposta agora',
+    '9' => 'Doar',
+    '10' => 'Aplicar aqui',
+    '11' => 'Citar aqui',
+    '12' => 'Pe&ccedil;a agora',
+    '13' => 'Reservar ingressos',
+    '14' => 'Inscreva-se agora',
+    '15' => 'Encontrar cart&ccedil;o',
+    '16' => 'Obter cita&ccedil;&atilde;o',
+    '17' => 'Obter bilhetes',
+    '18' => 'Localizar um distribuidor',
+    '19' => 'Pe&ccedil;a online',
+    '20' => 'Reserve agora',
+    '21' => 'Agende agora',
+    '22' => 'Inscreva-se agora',
+    '23' => 'Inscreva-se',
+    '24' => 'Registre'
+);
+
+$wo['products_categories'] = array(
+    '0' => 'All',
+    '1' => 'Apparel & Accessories',
+    '2' => 'Autos & Vehicles',
+    '3' => 'Baby & Children\'s Products',
+    '4' => 'Beauty Products & Services',
+    '5' => 'Computers & Peripherals',
+    '6' => 'Consumer Electronics',
+    '7' => 'Dating Services',
+    '8' => 'Financial Services',
+    '9' => 'Gifts & Occasions',
+    '10' => 'Home & Garden'
+);
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "globsex_globsex";
+// MySQL Database Password
+$sql_db_pass = "Euvouvencer2021";
+// MySQL Database Name
+$sql_db_name = "globsex_globsex";
+
+// Site URL
+$site_url = "https://globsex.com.br"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "fb2ceb6f-b479-473f-9b35-34b4508210c7"; // Your purchase code, don't give it to anyone. 
+
+$siteEncryptKey = '292affefb9ce57ce9d0a4bc9e34ad7f3fa8c1780';
+?>
